524ed8a051bf1abc5dc91f32058e69404ceaabae
Artem <artemzhdanov1299@gmail.com>
2023-11-27 00:34:29 +0200

    Add project files.

---
+++ hm11_10hm/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace hm11_10hm
+{
+    class Primer : INotifyPropertyChanged
+    {
+        private double a;
+        private double b;
+
+        public double A { get { return a; } set { a = value; onPropertyChanged("Число А"); } }
+        public double B { get { return b; } set { b = value; onPropertyChanged("Число Б"); } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Sum()
+        {
+            string ans = Convert.ToString(A + B);
+            return ans;
+        }
+
+        void onPropertyChanged(params string[] propertyNames)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                foreach (var pn in propertyNames)
+                {
+                    handler(this, new PropertyChangedEventArgs(pn));
+                }
+            }
+        }
+    }
+
+
+    public partial class MainWindow : Window
+    {
+        Primer primer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            primer = new Primer();
+            this.DataContext = primer;
+        }
+
+        private void result_GotFocus(object sender, RoutedEventArgs e)
+        {
+            result.Text = primer.Sum();
+        }
+    }
+}
